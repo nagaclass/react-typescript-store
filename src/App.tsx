120a79099186ab7536c1da6dfb31ca798370bf22
nagaclass <naga@nagaclass.com>
2022-06-05 11:13:33 +0200

    responsive time

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => {
   // Renders
   const renderProductListItems = () => {
     return (
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {data?.map(product => (
           <Product key={product.id} product={product} addToCartHandler={addToCartHandler} />
         ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "1rem",
+        "2xl": "6rem",
+      },
     },
   },
   plugins: [],
